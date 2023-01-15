a278befa54500b9e2fc18ba577faed78d0ce9734
António <oantoniomauricio@gmail.com>
2023-01-15 13:38:04 +0000

    responsive layout updated

--- scripts.js
+++ scripts.js
@@ -3,11 +3,53 @@ const h = 600;
 const barWidth = 0;
 const padding = 40;
 
+// RESPONSIVE
+function responsivefy(svg) {
+    // container will be the DOM element
+    // that the svg is appended to
+    // we then measure the container
+    // and find its aspect ratio
+    const container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.attr('width'), 10),
+        height = parseInt(svg.attr('height'), 10),
+        aspect = width / height;
+
+    // set viewBox attribute to the initial size
+    // control scaling with preserveAspectRatio
+    // resize svg on inital page load
+    svg.attr('viewBox', `0 0 ${width} ${height}`)
+        .attr('preserveAspectRatio', 'xMinYMid')
+        .call(resize);
+
+    // add a listener so the chart will be resized
+    // when the window resizes
+    // multiple listeners for the same event type
+    // requires a namespace, i.e., 'click.foo'
+    // api docs: https://goo.gl/F3ZCFr
+    d3.select(window).on(
+        'resize.' + container.attr('id'),
+        resize
+    );
+
+    // this is the code that resizes the chart
+    // it will be called on load
+    // and in response to window resizes
+    // gets the width of the container
+    // and resizes the svg to fill it
+    // while maintaining a consistent aspect ratio
+    function resize() {
+        const w = parseInt(container.attr('width'));
+        svg.attr('width', w);
+        svg.attr('height', Math.round(w / aspect));
+    }
+}
+
 // CREATE SVG
 const svg = d3.select("#holder")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
+    .call(responsivefy);
 
 //GET JSON DATA
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json", function (data) {
